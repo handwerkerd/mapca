33432c301fd69b6b6055a2cf31445a3ba3d0daa3
handwerkerd <dan.handwerker@gmail.com>
2023-01-09 15:30:41 -0500

    tried to add loggers to utils.py

--- mapca/utils.py
+++ mapca/utils.py
@@ -321,3 +321,38 @@ def _eigensp_adj(lam, n, p):
     lam_adj = lam / lam_emp
 
     return lam_adj
+
+
+
+def setup_loggers(logname=None, repname=None, quiet=False, debug=False):
+    # Set up the general logger
+    log_formatter = logging.Formatter(
+        "%(asctime)s\t%(module)s.%(funcName)-12s\t%(levelname)-8s\t%(message)s",
+        datefmt="%Y-%m-%dT%H:%M:%S",
+    )
+    stream_formatter = logging.Formatter(
+        "%(levelname)-8s %(module)s:%(funcName)s:%(lineno)d %(message)s"
+    )
+    # set up general logging file and open it for writing
+    if logname:
+        log_handler = logging.FileHandler(logname)
+        log_handler.setFormatter(log_formatter)
+        LGR.addHandler(log_handler)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(stream_formatter)
+    LGR.addHandler(stream_handler)
+
+    if quiet:
+        LGR.setLevel(logging.WARNING)
+    elif debug:
+        LGR.setLevel(logging.DEBUG)
+    else:
+        LGR.setLevel(logging.INFO)
+
+
+def teardown_loggers():
+    for local_logger in (LGR):
+        for handler in local_logger.handlers[:]:
+            handler.close()
+            local_logger.removeHandler(handler)
